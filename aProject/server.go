7f1a10a2bbfa5f66f41b1042425ddb26918ae8dd
mbjn <mbjn@itu.dk>
2022-10-30 16:06:20 +0100

    .

---
+++ aProject/server.go
@@ -0,0 +1,35 @@
+package mainers
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"chatserver"
+
+	grpc "github.com/mbjnitu/gRPC-exercise/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	Port := os.Getenv("PORT")
+	if Port == "" {
+		Port = "5000"
+	}
+
+	listen, err := net.Listen("tcp", ":"+Port)
+	if err != nil {
+		log.Fatalf("Could not listen @ %v :: %v", Port, err)
+	}
+	log.Println("Listening @ : " + Port)
+
+	grpcserver := grpc.NewServer(listen)
+
+	cs := chatserver.ChatServer{}
+	chatserver.RegisterServicesServer(grpcserver, &cs)
+
+	err = grpcserver.Serve(listen)
+	if err != nil {
+		log.Fatalf("Failed to start gRPC Server :: %v", err)
+	}
+}
